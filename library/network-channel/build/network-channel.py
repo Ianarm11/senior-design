944b0a31eb19c1934df30a892a099e8da0898eab
Ian Armstrong <17armstrongian@gmail.com>
2022-02-15 11:51:59 -0500

    Merged and modified receiver.py and sender.py with network-channel.py. Needs tested. Recieving side is stuck in a loop.

--- library/network-channel/build/network-channel.py
+++ library/network-channel/build/network-channel.py
@@ -1,31 +1,30 @@
 import sys
 import argparse
+import reciever
+import sender
 
 #TODO:
-#host_conver() for destination host
-
 #Server mode values vs Client mode values
 
 def main(argv):
-    arguments = parser()
-    print(getKey())
-    #makeCovertPacket(arguments)
-
-#def makeCovertPacket(args):
-    
+    args = parser()
+    #Reciever Main Function
+    reciever.receive_main(args.reciever_port, args.sender_host, args.reciever_host)
+    #Sender Main Function
+    sender.sender_main(args.reciever_port, createTCPPacket(args))
 
 def parser():
     parser = argparse.ArgumentParser(description='Process the inputs..')
 
     #Taking in all arguments from command line to be able to properly create the TCP packets for sending.
-    #Alternatively this is information needed to be able to recieve packets from another user. 
-    parser.add_argument('source_host', type=str, help='Source Host')
-    parser.add_argument('desination_host', type=str, help='Destination Host')
-    parser.add_argument('source_port', type=int, help='Source Port')
-    parser.add_argument('destination_port', type=int, help='Destination Port')\
-    parser.add_argument('--method', choices=['PID', 'SEQ', 'ACK'], default='PID', help='Choose between PID, Sequence, ACK')
-    parser.add_argument('-s', '--sender', type=bool, help='Sender')
-    parser.add_argument('file_name', type=str, help='File Name')
+    #Alternatively this is information needed to be able to recieve packets from another user.
+    parser.add_argument('sender_host', type=str, help='Sender Host')
+    parser.add_argument('reciever_host', type=str, help='Reciveing Host')
+    parser.add_argument('sender_port', type=int, help='Sender Port')
+    parser.add_argument('reciever_port', type=int, help='Reciever Port')\
+    #parser.add_argument('--method', choices=['PID', 'SEQ', 'ACK'], default='PID', help='Choose between PID, Sequence, ACK')
+    #parser.add_argument('-s', '--sender', type=bool, help='Sender')
+    #parser.add_argument('file_name', type=str, help='File Name')
 
     #parser.add_argument('file', type=int, help='File')
     #parser.add_argument('destination_host_string', type=string, help='Destination Host')
@@ -35,5 +34,58 @@ def parser():
 
     return args
 
+def createTCPPacket(args):
+    packet = TCPPacket(
+        args,
+        0b010101010  # Merry Christmas!
+    )
+    print(packet.flags)
+    print(packet.src_port)
+    return packet
+
+class TCPPacket:
+    def __init__(self, args, flags=0):
+        self.src_host = args.sender_host
+        self.src_port = args.sender_port
+        self.dst_host = args.reciever_host
+        self.dst_port = args.reciever_port
+        self.flags = flags
+
+    def build(self):
+
+        packet = struct.pack(
+            '!HHIIBBHHH',
+            self.src_port,  # Source Port
+            self.dst_port,  # Destination Port
+            0,              # Sequence Number
+            0,              # Acknoledgement Number
+            5 << 4,         # Data Offset
+            self.flags,     # Flags
+            8192,           # Window
+            0,              # Checksum (initial value)
+            0               # Urgent pointer
+        )
+
+        pseudo_hdr = struct.pack(
+            '!4s4sHH',
+            socket.inet_aton(self.src_host),    # Source Address
+            socket.inet_aton(self.dst_host),    # Destination Address
+            socket.IPPROTO_TCP,                 # Protocol ID
+            len(packet)                         # TCP Length
+        )
+
+        checksum = chksum(pseudo_hdr + packet)
+        packet = packet[:16] + struct.pack('H', checksum) + packet[18:]
+
+        return packet
+
+
+def chksum(packet):
+    if len(packet) % 2 != 0:
+        packet += b'\0'
+    res = sum(array.array("H", packet))
+    res = (res >> 16) + (res & 0xffff)
+    res += res >> 16
+    return (~res) & 0xffff
 
 main(sys.argv)
--- library/network-channel/build/reciever.py
+++ library/network-channel/build/reciever.py
@@ -3,33 +3,31 @@ import struct
 import collections
 
 
-def receive(port, source):
-    HOST = '127.0.0.1'
-    PORT = port
+def receive_main(destinationPort, sourceHost, destinationHost):
+    HOST = destinationHost
+    PORT = destinationPort
     IP_header = collections.namedtuple("IP_header", "ers_n_ihl srv_type total_len ip_id flags_n_frag_offset lifetime protocol header_chksum \
                      src_addr dest_addr opt_n_padding")
     TCP_header = collections.namedtuple("TCP_header", "src_port dst_port seq ack offset flags window checksum urgent")
     data_array = []
-    with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP) as s:
         s.bind((HOST, PORT))
-        s.listen(1)
-        conn, addr = s.accept()
-        if addr == source:
-            with conn:
-                while True:
-                    data = s.recv(65536)
-                    if not data:
-                        break
-                    start_index = 14  # I'm not sure this is correct, but it's something I found online; needs testing
-                    IP_header._make(struct.unpack("!BBHHHBBHIII", data[start_index:start_index+24]))
-                    # (src_port, dst_port, seq, ack, offset, flags, window, checksum, urgent) \
-                    TCP_header._make(struct.unpack("!HHIIBBHHHI", data[start_index+24:start_index+48]))
+        #s.listen(1)
+        #conn, addr = s.accept()
+        #if addr == sourceHost:
+        #with conn:
+        while True:
+            data = s.recvfrom(1024)
+            #data[0].decode()?
+            start_index = 14  # I'm not sure this is correct, but it's something I found online; needs testing
+            IP_header._make(struct.unpack("!BBHHHBBHIII", data[start_index:start_index+24]))
+            # (src_port, dst_port, seq, ack, offset, flags, window, checksum, urgent) \
+            TCP_header._make(struct.unpack("!HHIIBBHHHI", data[start_index+24:start_index+48]))
 
-                    data_array.append((IP_header, TCP_header, data[48:]))
+            data_array.append((IP_header, TCP_header, data[48:]))
 
     return data_array
 
 
-if __name__ == '__main__':
-    receive(666, '127.0.0.2')
-
+#if __name__ == '__main__':
+    #receive(666, '127.0.0.2')
